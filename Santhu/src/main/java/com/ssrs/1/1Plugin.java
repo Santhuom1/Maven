d471730a046d4fed88989c34836498b0a3c1638b
Santhu <santhuom@gmail.com>
2022-10-08 06:36:45 +0530

    testmaven

---
+++ Santhu/src/main/java/com/ssrs/1/1Plugin.java
@@ -0,0 +1,19 @@
+package com.ssrs.1;
+
+import com.ssrs.framework.extend.plugin.AbstractPlugin;
+import com.ssrs.framework.extend.plugin.PluginException;
+
+/**
+ * @author ssrs
+ */
+public class 1Plugin extends AbstractPlugin {
+    @Override
+    public void start() throws PluginException {
+
+    }
+
+    @Override
+    public void stop() throws PluginException {
+
+    }
+}
